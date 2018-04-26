36f9bfd20d2d23fbb8e43f5b65f7ceaa724c5a90
MichaelJamesParsons <mjay.parsons@gmail.com>
2018-04-25 23:22:40 -0400

    Implemented dummy endpoints

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express'
 import http from 'http'
 import io from 'socket.io'
 import md5 from 'md5'
+import JsonWebToken from 'jsonwebtoken'
+import bodyParser from 'body-parser'
 import models from './models'
 
 const app = express()
@@ -12,16 +14,57 @@ const EVENT_SCOPE_MESSAGE = 'message'
 const EVENT_SCOPE_SESSION = 'session'
 
 app.use('/scripts', express.static(__dirname + '/../node_modules'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/create-conversation', (req, res) => {
+app.post('/auth/register', (req, res) => {
+  res.json({
+    "success": true
+  })
+})
+
+app.post('/auth/login', (req, res) => {
+  const jwt = JsonWebToken.sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+    data: {
+      user: 'test'
+    }
+  }, 'secret')
+
+  res.json({
+    "jwt": jwt
+  })
+})
+
+app.get('/conversations', (req, res) => {
+  res.json({
+    "conversations": [
+      {
+        "label": "test1",
+        "token": "aaaaaa"
+      },
+      {
+        "label": "test2",
+        "token": "bbbbb"
+      },
+      {
+        "label": "test3",
+        "token": "ccccc"
+      }
+    ]
+  })
+})
+
+app.post('/conversations/create', (req, res) => {
   models.conversation.create({
     token: md5((new Date()).getTime()).substring(0, 5)
   }).then((conversation) => {
     res.json({
+      label: req.body.label,
       token: conversation.token
     })
   })
@@ -31,7 +74,7 @@ wsServer.on('connection', (socket) => {
   console.log('connected')
 
   socket.on('event', (event) => {
-    var message = {
+    const message = {
       scope: EVENT_SCOPE_MESSAGE,
       type: event.type,
       content: event.content,
