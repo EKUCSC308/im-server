60625c72b4b29cd24385dc7d8eb19d28602948bb
Michael-James Parsons <mjay.parsons@gmail.com>
2018-05-02 15:10:43 -0400

    Finished HTTP endpoints

--- database/migrations/20180410192803-create-conversation.js
+++ database/migrations/20180410192803-create-conversation.js
@@ -11,6 +11,9 @@ module.exports = {
       token: {
         type: Sequelize.STRING
       },
+      label: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ import express from 'express'
 import http from 'http'
 import io from 'socket.io'
 import md5 from 'md5'
+import _ from 'lodash'
 import JsonWebToken from 'jsonwebtoken'
 import bodyParser from 'body-parser'
 import models from './models'
+import * as passwordHelper from './crypto/password'
 
 const app = express()
 const server = http.createServer(app)
@@ -21,18 +23,60 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 })
 
-app.post('/auth/register', (req, res) => {
+app.post('/auth/register', async (req, res) => {
+  if (_.isEmpty(req.body.username) || _.isEmpty(req.body.password)) {
+    return res.json({
+      success: false,
+      error: "Username and password must have value."
+    })
+  }
+
+  const user = await models.user.findOne({ where: { username: req.body.username }})
+
+  if (!_.isEmpty(user)) {
+    return res.json({
+      success: false,
+      error: "An account with that username already exists."
+    })
+  }
+
+  await models.user.create({
+    username: req.body.username,
+    password: passwordHelper.hashPassword(req.body.password)
+  })
+
   res.json({
     "success": true
   })
 })
 
-app.post('/auth/login', (req, res) => {
+app.post('/auth/login', async (req, res) => {
+  if (_.isEmpty(req.body.username) || _.isEmpty(req.body.password)) {
+    return res.json({
+      success: false,
+      error: "Username and password must have value."
+    })
+  }
+
+  const user = await models.user.findOne({
+    where: {
+      username: req.body.username,
+      password: passwordHelper.hashPassword(req.body.password)
+    }
+  })
+
+  if (!user) {
+    return res.json({
+      success: false,
+      error: 'Invalid username and password.'
+    })
+  }
+
+  delete user.password
+
   const jwt = JsonWebToken.sign({
     exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-    data: {
-      user: 'test'
-    }
+    data: user
   }, 'secret')
 
   res.json({
@@ -40,27 +84,29 @@ app.post('/auth/login', (req, res) => {
   })
 })
 
-app.get('/conversations', (req, res) => {
-  res.json({
-    "conversations": [
-      {
-        "label": "test1",
-        "token": "aaaaaa"
-      },
-      {
-        "label": "test2",
-        "token": "bbbbb"
-      },
-      {
-        "label": "test3",
-        "token": "ccccc"
-      }
-    ]
-  })
+app.get('/conversations', async (req, res) => {
+  await models.conversation.findAll()
+    .then((conversations) => {
+      res.json({
+        "conversations": conversations.map((conversation) => {
+          return {
+            token: conversation.token,
+            label: conversation.label
+          }
+        })
+      })
+    })
+    .catch(() => {
+      return res.json({
+        success: false,
+        error: "Failed to load conversations."
+      })
+    })
 })
 
 app.post('/conversations/create', (req, res) => {
   models.conversation.create({
+    label: req.body.label,
     token: md5((new Date()).getTime()).substring(0, 5)
   }).then((conversation) => {
     res.json({
@@ -92,4 +138,4 @@ wsServer.on('connection', (socket) => {
   })
 })
 
-server.listen(3001);
+server.listen(3001)
